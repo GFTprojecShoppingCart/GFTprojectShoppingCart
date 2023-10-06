3d20db868717860b3c58cfae5798d116d9e5a31d
joelvp <joelvalpe8@gmail.com>
2023-10-06 12:42:57 +0200

    First Integration Test Working

--- src/main/java/com/gftproject/shoppingcart/model/Cart.java
+++ src/main/java/com/gftproject/shoppingcart/model/Cart.java
@@ -2,7 +2,13 @@ package com.gftproject.shoppingcart.model;
 
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +17,10 @@ import lombok.Setter;
 @Entity
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Enumerated(EnumType.STRING) // Use EnumType.STRING to map the enum by its name
     private Status status;
 }
