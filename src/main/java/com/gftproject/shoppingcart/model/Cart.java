262df6f59f8a603ee7662f593586735a765b7497
Pau <uapcent@gmail.com>
2023-10-10 09:03:20 +0200

    Added Controller implementation and tests

--- src/main/java/com/gftproject/shoppingcart/model/Cart.java
+++ src/main/java/com/gftproject/shoppingcart/model/Cart.java
@@ -1,14 +1,7 @@
 package com.gftproject.shoppingcart.model;
 
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
