73a1e79c262aeacb64e708af2079772271b3ada1
Sergio <selojor22@gmail.com>
2023-10-20 09:06:58 +0200

    ADDED some GIVEN WHEN THEN and REPLACED Autowired in repository and ADDED computeByWeight for additional costs

--- src/test/java/com/gftproject/shoppingcart/repositories/ShoppingCartRepositoryTest.java
+++ src/test/java/com/gftproject/shoppingcart/repositories/ShoppingCartRepositoryTest.java
@@ -1,17 +1,14 @@
 package com.gftproject.shoppingcart.repositories;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import com.gftproject.shoppingcart.model.Product;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -21,21 +18,12 @@ import com.gftproject.shoppingcart.model.Status;
 @DataJpaTest
 public class ShoppingCartRepositoryTest {
 
-    private ShoppingCartRepository shoppingCartRepository;
-    @Mock
-    private ShoppingCartRepository mockRepository;
-
-    @BeforeEach
-    void setUp(){
-        shoppingCartRepository = mockRepository;
-    }
+    @Autowired
+    ShoppingCartRepository shoppingCartRepository;
 
     @Test
     @DisplayName("GIVEN a status WHEN testFindAllByStatus THEN find all carts by status")
     void testFindAllByStatus() {
-        List<Cart> cartList = List.of(new Cart(), new Cart());
-        when(mockRepository.findAllByStatus(Status.DRAFT)).thenReturn(cartList);
-
         List<Cart> cart = shoppingCartRepository.findAllByStatus(Status.DRAFT);
 
         assertFalse(cart.isEmpty());
@@ -46,12 +34,10 @@ public class ShoppingCartRepositoryTest {
     @Test
     @DisplayName("GIVEN cartId WHEN deleteCart THEN delete a cart object")
     void deleteCart() {
-        Cart cart = new Cart();
-        when(mockRepository.findById(1L)).thenReturn(Optional.of(cart));
+        Cart cart = shoppingCartRepository.findById(1L).orElseThrow();
+        assertEquals(1L, cart.getId());
 
         shoppingCartRepository.delete(cart);
-
-        when(mockRepository.findById(1L)).thenReturn(Optional.empty());
 
         assertThrows(NoSuchElementException.class, () -> {
             shoppingCartRepository.findById(1L).orElseThrow();
