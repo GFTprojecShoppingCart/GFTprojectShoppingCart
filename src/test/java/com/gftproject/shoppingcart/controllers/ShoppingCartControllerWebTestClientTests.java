228ba88bfb569d54412c78cf830f0a04c52e1a0d
isla <iileo.c.1@gmail.com>
2023-10-20 09:46:55 +0200

    UPDATED getAllById test - STATUS: green

--- src/test/java/com/gftproject/shoppingcart/controllers/ShoppingCartControllerWebTestClientTests.java
+++ src/test/java/com/gftproject/shoppingcart/controllers/ShoppingCartControllerWebTestClientTests.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,14 +27,11 @@ class ShoppingCartControllerWebTestClientTests {
     void setUp() {
         ObjectMapper objectMapper = new ObjectMapper();
     }
-
+    String userId = "1";
     @Test
     @Order(1)
-    @DisplayName("GIVEN an user WHEN a cart is created")
+    @DisplayName("GIVEN an user id WHEN a cart is created")
     void createCartTest() {
-        // Given
-        //defino la variable userId para quien se creará la cart.
-        String userId = "1";
 
         // When
         client.post().uri("/carts/" + userId)
@@ -47,12 +43,12 @@ class ShoppingCartControllerWebTestClientTests {
 
     @Test
     @Order(2)
-    @DisplayName("GIVEN userId WHEN findAllByStatus is executed THEN list carts by status")
+    @DisplayName("GIVEN user id WHEN findAllByStatus is executed THEN list carts by status")
 
     void findAllByUserId() {
         final List<Cart> carts = new ArrayList<>(); // Declarar como final y efectivamente final
 
-        client.get().uri("/carts/1").exchange()
+        client.get().uri("/carts/" + userId).exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(MediaType.APPLICATION_JSON)
                 .expectBodyList(Cart.class)
@@ -63,27 +59,28 @@ class ShoppingCartControllerWebTestClientTests {
         assertNotNull(carts);
         assertEquals(2, carts.size()); // Verificar que haya 2 registros en la lista import.sql del usuario 1
 
-// Verificar el primer registro del user id 1
+        // Verificar el primer registro del user id 1
         assertEquals(1L, carts.get(0).getId());
         assertEquals("DRAFT", carts.get(0).getStatus().getStatus());
         assertEquals(0, carts.get(0).getFinalPrice().intValue());
         assertEquals(0, carts.get(0).getFinalWeight().intValue());
 
-// Verificar el tercer registro del user id 1
+        // Verificar el tercer registro del user id 1
         assertEquals(3L, carts.get(1).getId());
         assertEquals("SUBMITTED", carts.get(1).getStatus().getStatus());
         assertEquals("4.50", carts.get(1).getFinalPrice().toPlainString());
         assertEquals(0, carts.get(1).getFinalWeight().intValue());
 
-
-//        client.get().uri("/carts/1").exchange()
-//                .expectStatus().isOk()
-//                .expectHeader().contentType(APPLICATION_JSON)
-//                .expectBody()
-//                .jsonPath("$[0].cart").isEqualTo("SUMMITED")
-//                .jsonPath("$[0].cart").isEqualTo("DRAFT")
-//                .jsonPath("$[1].id").isEqualTo("1");
+        client.get().uri("/carts/" + userId).exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$[0].status").isEqualTo("DRAFT")      // Verificar el estado del primer carrito
+                .jsonPath("$[1].status").isEqualTo("SUBMITTED")  // Verificar el estado del segundo carrito
+                .jsonPath("$[0].id").isEqualTo(1L)      // Verificar el ID del primer carrito
+                .jsonPath("$[1].id").isEqualTo(3L);      // Verificar el ID del segundo carrito
     }
+
 
         @Test
         @Order(3)         //Este ha de ser el último test que corra
@@ -107,11 +104,5 @@ class ShoppingCartControllerWebTestClientTests {
                     .expectHeader().contentType(MediaType.APPLICATION_JSON)
                     .expectBodyList(Cart.class)
                     .hasSize(2);//Una vez eliminado el carrito, esperamos que haya 1 menos en la lista
-
-//            client.get().uri("/carts/1").exchange()
-//                    .expectStatus().isNotFound();
-
     }
-
-
 }
